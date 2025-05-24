2ec28c45e036c80d033bbc2b03f9ced18eb0911f
35505126 <35505126@student.murdoch.edu.au>
2025-05-25 00:18:34 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,9 @@
 <?php
-// html/database.php
-$host   = '127.0.0.1';
-$db     = 'jadey_bakery_database';
+$dsn  = 'mysql:host=127.0.0.1;dbname=jadeys_bakery;charset=utf8mb4';
 $user   = 'username';
 $pass   = 'password';
 $charset= 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
